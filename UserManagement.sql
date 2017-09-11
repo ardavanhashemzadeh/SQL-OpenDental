1b07bbcad1d748f84a3080894d8d838f58ea21bf
ardavanhashemzadeh <ardavanhashemzadeh@users.noreply.github.com>
2017-09-11 19:14:43 -0400

    Update UserManagement.sql

--- UserManagement.sql
+++ UserManagement.sql
@@ -1,29 +1,22 @@
-/*
-** Ardavan Hashemzadeh
+/* Ardavan Hashemzadeh
 ** September 5 2017
-** Insert or Update OD user
-** Will create a usergroup if it doesn't exist already
-*/
-
+** Insert or Update OD user creating usergroup
+** if it doesn't exist already */
 SET @olduname='';
 SET @username='';
 SET @password='';
 SET @usergroup='';
-
 INSERT INTO usergroup(Description)
 SELECT @usergroup AS Description FROM (SELECT NULL) PlaceHolder
 WHERE NOT EXISTS (SELECT Description FROM usergroup WHERE Description LIKE @usergroup);
-
 INSERT INTO userod(username, password, usergroupnum)
 SELECT @username AS username, @password AS password,
 (SELECT usergroupnum FROM usergroup WHERE description LIKE @usergroup LIMIT 1) AS usergroupnum
-FROM (SELECT NULL) PlaceHolder
-WHERE @username<>'' AND NOT EXISTS
+FROM (SELECT NULL) PlaceHolder WHERE @username<>'' AND NOT EXISTS
 (SELECT username FROM userod WHERE username=@username UNION SELECT username FROM userod WHERE username=@olduname);
-
 UPDATE userod SET ishidden=0 WHERE username=@olduname OR username=@username;
 UPDATE userod SET username=@username WHERE @olduname<>'' AND username=@olduname;
 UPDATE userod SET password=@password WHERE @password<>'' AND username=@username;
-UPDATE userod SET usergroupnum=(SELECT usergroupnum FROM usergroup WHERE description LIKE @usergroup LIMIT 1) WHERE @usergroup<>'' AND username = @username;
-
+UPDATE userod SET usergroupnum=(SELECT usergroupnum FROM usergroup WHERE description LIKE @usergroup LIMIT 1)
+WHERE @usergroup<>'' AND username = @username;
 SELECT ishidden, username, password, description FROM userod NATURAL LEFT JOIN usergroup WHERE username LIKE @username;
