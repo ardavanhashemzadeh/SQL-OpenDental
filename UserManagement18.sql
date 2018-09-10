79563076212ebb9d771afa8c030595dcf6dacd64
ardavanhashemzadeh <ardavanhashemzadeh@users.noreply.github.com>
2018-09-10 16:05:30 -0400

    Update UserManagement18.sql

--- UserManagement18.sql
+++ UserManagement18.sql
@@ -2,18 +2,21 @@
 ** September 10 2018
 ** Insert or Update OD user creating usergroup
 ** if it doesn't exist already */
+
 SET @olduname='';
 SET @username='';
 SET @password='';
 SET @usergroup='';
+
 INSERT INTO usergroup(Description)
 SELECT @usergroup AS Description FROM (SELECT NULL) PlaceHolder
 WHERE NOT EXISTS (SELECT Description FROM usergroup WHERE Description LIKE @usergroup);
-INSERT INTO userod(username, password, usergroupnum)
-SELECT @username AS username, @password AS password,
-(SELECT usergroupnum FROM usergroup WHERE description LIKE @usergroup LIMIT 1) AS usergroupnum
+
+INSERT INTO userod(username)
+SELECT @username AS username
 FROM (SELECT NULL) PlaceHolder WHERE @username<>'' AND NOT EXISTS
 (SELECT username FROM userod WHERE username=@username UNION SELECT username FROM userod WHERE username=@olduname);
+
 UPDATE userod SET ishidden=0 WHERE username=@olduname OR username=@username;
 UPDATE userod SET username=@username WHERE @olduname<>'' AND username=@olduname;
 UPDATE userod SET password=@password WHERE @password<>'' AND username=@username;
