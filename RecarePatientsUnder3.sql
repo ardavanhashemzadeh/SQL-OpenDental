675743c39c261c12e6cad9489b4fe9afdf84be45
ardavanhashemzadeh <ardavanhashemzadeh@users.noreply.github.com>
2018-09-25 14:50:10 -0400

    Update RecarePatientsUnder3.sql

--- RecarePatientsUnder3.sql
+++ RecarePatientsUnder3.sql
@@ -1,1 +1,15 @@
+/*  Ardavan Hashemzadeh
+    September 25 2018
+    List patients under 3yo
+    who are due for recare
+*/
 
+SELECT patnum, pat, age
+FROM (
+  SELECT patnum,
+  TIMESTAMPDIFF(YEAR,birthdate,CURDATE()) AS age,
+  CONCAT(fname, " ", lname) AS pat,
+  MAX(aptdatetime) AS LastApt
+  FROM appointment NATURAL JOIN patient) a
+WHERE DATE(LastApt) <= DATE_SUB(now(), INTERVAL 6 MONTH)
+AND age <= 3;
