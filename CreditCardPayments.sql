76beffe790bc9d5b2545555406cd12750bfca80f
ardavanhashemzadeh <ardavanhashemzadeh@users.noreply.github.com>
2018-10-17 17:53:16 -0400

    Update and rename payments.sql to CreditCardPayments.sql

--- CreditCardPayments.sql
+++ CreditCardPayments.sql
@@ -1,4 +1,5 @@
 SELECT paydate, payamt, itemname
 FROM payment p JOIN definition d ON p.PayType=d.defnum
 WHERE paydate BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()
+AND itemname LIKE "%credit%"
 ORDER BY paydate DESC;
