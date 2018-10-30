f6accdc6faa2467aed84f26cddab259dce7691df
ardavanhashemzadeh <ardavanhashemzadeh@users.noreply.github.com>
2018-10-30 11:57:10 -0400

    Update CreditCardPayments.sql

--- CreditCardPayments.sql
+++ CreditCardPayments.sql
@@ -1,5 +1,10 @@
-SELECT paydate, payamt, itemname
-FROM payment p JOIN definition d ON p.PayType=d.defnum
-WHERE paydate BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()
+/* Ardavan Hashemzadeh
+   October 16 2018
+   Show patient ayments for last month
+   Broken down by payment type
+*/
+SELECT paydate, itemname, payamt
+FROM payment p JOIN definition d ON p.paytype=d.defnum
+WHERE paydate BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) and NOW()
 AND itemname LIKE "%credit%"
-ORDER BY paydate DESC;
+ORDER BY paydate DESC
