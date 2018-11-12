ae9eee2bca9d94b3979241c358b30de05e950aa7
ardavanhashemzadeh <ardavanhashemzadeh@users.noreply.github.com>
2018-11-12 14:01:57 -0500

    Update November122018.sql

--- November122018.sql
+++ November122018.sql
@@ -6,13 +6,101 @@ SET @MinAge=0;
 SET @MaxAge=3;
 
 SELECT patnum, Age, LastAppointment FROM (
-        SELECT pl.patnum, YEAR(NOW())-YEAR(Birthdate) AS Age, MAX(aptdatetime) AS LastAppointment FROM patient p JOIN procedurelog pl ON p.patnum=pl.patnum
-        JOIN procedurecode pc ON pl.codenum=pc.codenum
+        SELECT pl.patnum, YEAR(NOW())-YEAR(Birthdate) AS Age, MAX(aptdatetime) AS LastAppointment
+        FROM patient p
+        JOIN procedurelog pl ON p.patnum=pl.patnum
         LEFT JOIN appointment a ON pl.patnum=a.patnum
         WHERE procdate >= DATE_SUB(NOW(), INTERVAL 3 YEAR)
         AND procstatus=1
         GROUP BY pl.patnum) a
 WHERE (LastAppointment <= NOW() OR LastAppointment IS NULL)
-AND Age <=18
+AND Age BETWEEN @MinAge AND @MaxAge
 ORDER BY LastAppointment DESC
 
+/*
+*
+*
+*
+*
+*/
+
+
+/*  Ardavan Hashemzadeh
+    November 12 2018
+    List of patients with No Bundles/Codes generated for the appointment
+    This query returns patients who have planned appointments with no procedures
+*/
+SET @MinAge=0;
+SET @MaxAge=3;
+
+SELECT patnum, Age, LastAppointment FROM (
+        SELECT pl.patnum, YEAR(NOW())-YEAR(Birthdate) AS Age, MAX(aptdatetime) AS LastAppointment
+        FROM patient p
+        JOIN procedurelog pl ON p.patnum=pl.patnum
+        LEFT JOIN appointment a ON pl.patnum=a.patnum
+        GROUP BY pl.patnum) a
+JOIN (SELECT patnum FROM procedurelog pl WHERE aptnum IS NULL GROUP BY patnum) b ON a.patnum=b.patnum
+WHERE (LastAppointment <= NOW() OR LastAppointment IS NULL)
+AND Age BETWEEN @MinAge AND @MaxAge
+ORDER BY LastAppointment DESC
+/*
+*
+*
+*
+*
+*/
+
+
+
+/*  Ardavan Hashemzadeh
+    November 12 2018
+    List of patients with No re-Care appointment is made
+    This query returns patients who do not have future planned appointments
+*/
+SET @MinAge=0;
+SET @MaxAge=3;
+
+SELECT patnum, Age, LastAppointment FROM (
+        SELECT pl.patnum, YEAR(NOW())-YEAR(Birthdate) AS Age, MAX(aptdatetime) AS LastAppointment
+        FROM patient p
+        JOIN procedurelog pl ON p.patnum=pl.patnum
+        LEFT JOIN appointment a ON pl.patnum=a.patnum
+        GROUP BY pl.patnum) a
+WHERE (LastAppointment <= NOW() OR LastAppointment IS NULL)
+AND Age BETWEEN @MinAge AND @MaxAge
+ORDER BY LastAppointment DESC
+/*
+*
+*
+*
+*
+*/
+
+
+
+
+
+/*  Ardavan Hashemzadeh
+    November 12 2018
+    List of patients with Spacemaintainer Impression Appointment is not made
+    In Progress
+*/
+SET @MinAge=0;
+SET @MaxAge=3;
+
+SELECT patnum, Age, LastAppointment FROM (
+        SELECT pl.patnum, YEAR(NOW())-YEAR(Birthdate) AS Age, MAX(aptdatetime) AS LastAppointment
+        FROM patient p
+        JOIN procedurecode pc ON pc.codenum=pl.proccode
+        JOIN procedurelog pl ON p.patnum=pl.patnum
+        LEFT JOIN appointment a ON pl.patnum=a.patnum
+        GROUP BY pl.patnum) a
+WHERE (LastAppointment <= NOW() OR LastAppointment IS NULL)
+AND Age BETWEEN @MinAge AND @MaxAge
+ORDER BY LastAppointment DESC
+/*
+*
+*
+*
+*
+*/
