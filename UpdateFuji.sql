853433a37f28f0a7efb8ec1fc396d4e98d4a1a3f
ardavanhashemzadeh <ardavanhashemzadeh@users.noreply.github.com>
2019-05-21 17:58:43 -0400

    Update UpdateFuji.sql

--- UpdateFuji.sql
+++ UpdateFuji.sql
@@ -3,28 +3,27 @@
 -- Update new code for "Fuji"
 
 
-UPDATE procedurecode Old, procedurecode New
-SET New.ProcCode=@NewCode,
-New.Descript="Filling - Fuji",
+UPDATE procedurecode AS OldTbl, procedurecode AS NewTbl
+SET New.Descript="Filling - Fuji",
 New.AbbrDesc="Fill Fuji",
-New.ProcTime=,
-ProcCat,
-TreatArea,
-NoBillIns,
-IsProsth,
-DefaultNote,
-IsHygiene,
-GTypeNum,
-IsTaxed,
-PaintType,
--256 AS GraphicColor,
-IsCanadianLab,
-PreExisting,
-BaseUnits,
-SubstOnlyIf,
-IsMultiVisit,
-ProvNumDefault,
-CanadaTimeUnits,
-IsRadiology,
-BypassGlobalLock
-FROM procedurecode WHERE ProcCode=@OldCode
+New.ProcTime=Old.ProcTime,
+New.ProcCat=Old.ProcCat,
+New.TreatArea=Old.TreatArea,
+New.NoBillIns=Old.NoBillIns,
+New.IsProsth=Old.IsProsth,
+New.DefaultNote=Old.DefaultNote,
+New.IsHygiene=Old.IsHygiene,
+New.GTypeNum=Old.GTypeNum,
+New.IsTaxed=Old.IsTaxed,
+New.PaintType=Old.PaintType,
+New.GraphicColor=-256,
+New.IsCanadianLab=Old.IsCanadianLab,
+New.PreExisting=Old.PreExisting,
+New.BaseUnits=Old.BaseUnits,
+New.SubstOnlyIf=Old.SubstOnlyIf,
+New.IsMultiVisit=Old.IsMultiVisit,
+New.ProvNumDefault=Old.ProvNumDefault,
+New.CanadaTimeUnits=Old.CanadaTimeUnits,
+New.IsRadiology=Old.IsRadiology,
+New.BypassGlobalLock=Old.BypassGlobalLock
+WHERE Old.ProcCode=@OldCode AND New.ProcCode=@NewCode;
