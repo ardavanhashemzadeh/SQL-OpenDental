d42e6aa5e32f877f9bee1c0b48afc93fbb55929d
ardavanhashemzadeh <ardavanhashemzadeh@users.noreply.github.com>
2019-06-04 16:08:31 -0400

    Create QuestionableCardTransactions.sql

---
+++ QuestionableCardTransactions.sql
@@ -0,0 +1,11 @@
+-- Ardavan Hashemzadeh
+-- June 4th 2018
+-- Show number of card transactions with no note from past month
+-- Broken down by user and day
+
+SELECT paydate, itemname, payamt, username
+FROM payment p JOIN definition d ON p.paytype=d.defnum JOIN userod USING(usernum)
+WHERE paydate BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) and NOW()
+AND itemname LIKE "%credit%"
+GROUP BY paydate, username
+ORDER BY paydate DESC
