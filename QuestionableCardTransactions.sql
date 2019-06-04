fa2e3bdfa437e41fd5b81b3979facb7bca6b1569
ardavanhashemzadeh <ardavanhashemzadeh@users.noreply.github.com>
2019-06-04 16:27:34 -0400

    Update QuestionableCardTransactions.sql

--- QuestionableCardTransactions.sql
+++ QuestionableCardTransactions.sql
@@ -1,12 +1,12 @@
 -- Ardavan Hashemzadeh
 -- June 4th 2018
--- Show number of card transactions with no note from past month
+-- Show number of card transactions with no note from past year
 -- Broken down by user and day
 
 SELECT username, paydate, itemname, paynote /*, COUNT(*) AS 'Instances', SUM(PayAmt) AS 'Amount' */
 FROM payment p JOIN definition d ON p.paytype=d.defnum JOIN userod u ON p.SecUserNumEntry=u.usernum
-WHERE paydate BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) and NOW()
+WHERE paydate BETWEEN DATE_SUB(NOW(), INTERVAL 1 YEAR) and NOW()
 AND itemname LIKE "%credit%"
-AND paynote IS NULL OR paynote = ''
+AND paynote = ''
 GROUP BY paydate, username
 ORDER BY paydate DESC
