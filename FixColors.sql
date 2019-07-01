27c47d10f7c5f248d7e6329dd059510b500c713f
ardavanhashemzadeh <ardavanhashemzadeh@users.noreply.github.com>
2019-07-01 16:06:36 -0400

    Create FixColors.sql

---
+++ FixColors.sql
@@ -0,0 +1,10 @@
+-- Fix issues caused by blind automation
+
+
+-- Fix treat area of fillings
+UPDATE procedurecode SET TreatArea=1,PaintType=6 WHERE proccode IN ('D2940F', 'D2940', 'N2940');
+
+
+-- Inform OD of the changes
+SET @DTS=(SELECT NOW());
+INSERT INTO signalod (DateViewing,SigDateTime,FKey,FKeyType,IType,RemoteRole,MsgValue) VALUES('0001-01-01',@DTS,0,'Undefined',4,0,'');
