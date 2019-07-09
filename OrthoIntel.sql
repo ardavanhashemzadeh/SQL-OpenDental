4e2a3999c01513fdaf9de10de3d4a127c74f07ec
ardavanhashemzadeh <ardavanhashemzadeh@users.noreply.github.com>
2019-07-09 19:02:48 -0400

    Update OrthoIntel.sql

--- OrthoIntel.sql
+++ OrthoIntel.sql
@@ -1,3 +1,20 @@
+SELECT StartYear, StartQuarter, Starts.FeeSchedule, COUNT(Starts.patnum) AS Starts, COUNT(Debands.patnum) AS Completed
+FROM (
+  SELECT DISTINCT p.patnum, fs.description AS FeeSchedule, YEAR(procdate) AS StartYear, QUARTER(procdate) AS StartQuarter
+  FROM procedurelog JOIN procedurecode USING(codenum)
+  JOIN patient p USING(patnum)
+  JOIN feesched fs ON p.feesched=fs.feeschednum
+  WHERE procstatus=2
+  AND proccode IN ("D8060", "D8080", "D8090")) Starts
+LEFT JOIN (
+  SELECT DISTINCT patnum
+  FROM procedurelog JOIN procedurecode USING(codenum)
+  WHERE procstatus=2
+  AND proccode="D8680") Debands
+USING(patnum)
+GROUP BY StartYear, StartQuarter, FeeSchedule
+ORDER BY StartYear, StartQuarter, FeeSchedule
+
 SET @FromDate='2018-01-01', @ToDate='2018-12-31';
 SELECT Starts.FeeSchedule, COUNT(Starts.patnum) AS Starts, COUNT(Debands.patnum) AS Completed
 FROM (
