cb89e6834e6fadedbf6ad281c1a55da0bfd200a3
ardavanhashemzadeh <ardavanhashemzadeh@users.noreply.github.com>
2019-07-10 09:55:14 -0400

    Update OrthoIntel.sql

--- OrthoIntel.sql
+++ OrthoIntel.sql
@@ -3,7 +3,7 @@
 SELECT ProcCode, Amount, Description AS FeeSchedule
 FROM fee f LEFT JOIN feesched fs ON f.feesched=fs.feeschednum
 LEFT JOIN procedurecode USING(codenum)
-JOIN (SELECT DISTINCT codenum FROM procedurecode WHERE procstatus=2) USING(codenum)
+JOIN (SELECT DISTINCT codenum FROM procedurecode WHERE procstatus=2) CodesUsed USING(codenum)
 ORDER BY FeeSchedule
 
 
